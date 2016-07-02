42da117d8b459bcad6e5881d667510c8c3c19230
giTomas <tomas.kosegi@gmail.com>
2016-07-02 11:33:25 +0200

    all is working

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ var sass         = require('gulp-sass');
 
 
 gulp.task('sass', function () {
-    return gulp.src('sass/main.scss')
+    return gulp.src('assets/css/main.scss')
         .pipe(sass({
             includePaths: ['css']
         }))
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('production'))
-        .pipe(gulp.dest('sass'));
+        // .pipe(gulp.dest('sass'));
 });
 
 gulp.task('browserSync', function() {
@@ -34,19 +34,19 @@ gulp.task('pug', function(){
     }))
     .pipe(gulp.dest('production'))
 });
-
+/*
 gulp.task('prettify', function(){
   gulp.src('js/*.js')
     .pipe(prettify({collapseWhitespace: true}))
     .pipe(gulp.dest('production/js'))
- });
+ });*/
 
 gulp.task('watch', ['browserSync'], function(){
-  gulp.watch('assets/css/*.css', ['styles']);
+  // gulp.watch('assets/css/*.scss', ['saas']);
   gulp.watch('views/*.pug', ['pug']);
-  gulp.watch('js/*.js', ['prettify']);
+  // gulp.watch('js/*.js', ['prettify']);
   gulp.watch('production/**', browserSync.reload);
-  gulp.watch('assets/css/*.sass', ['sass']);
+  gulp.watch('assets/css/**', ['sass']);
 });
 
 gulp.task('default', ['browserSync', 'watch']);
--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 (function() {
   "use strict";
 
+  console.log('work');
+
+  const signup          = document.getElementById('js-singup');
+  const usernameUsed    = document.getElementById('js-username-used');
+  const usernameFree    = document.getElementById('js-username-free');
+  const passwordInvalid = document.getElementById('js-password-invalid');
+
+  const handler = (event) => {
+    console.log(event);
+    event.preventDefault();
+  }
+
+  console.log(signup);
+  signup.addEventListener('submit', handler, false);
+
+  // signup.addEventListener('submit', (event) => console.log('click'))
+    // event.preventDefault();
+    // console.log('clicK')
+
+    // const formData = new formData('signup');
+    // const password = formData.get('password');
+    // const passwordRepeat = formData.get('passwordrepeat');
+    // if (password === passwordRepeat) {
+      // console.log('password is ok');
+      // formData.delete('passwordrepeat');
+    // } else {
+      // console.log("passwords don't agree")
+    // }
+
+  // });
 
 
 
-})(); //end iffy
+
+})();
